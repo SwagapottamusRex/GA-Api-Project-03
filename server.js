0bb6f79d2b3ecafe35b49dd6968668bd6bf51ec9
Oscar <oscarmdial@gmail.com>
2022-03-23 16:19:22 +0000

    Starter Code

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 import express from "express";
+import { port } from './config/environment.js';
+import { connectToDb } from './db/helpers.js';
+import router from './config/router.js';
+
+
+
 const app = express();
+app.use(express.json());
+app.use('/api', router);
+
+async function runServer() {
+  console.log('Connecting to the db..');
+  await connectToDb();
+  console.log('Connected to the db sucessfully!');
+  app.listen(port);
+}
+runServer();
